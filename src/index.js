56914d95083030dab32e35b78591185e40f4dad0
kiterumer <1404646092@qq.com>
2019-05-05 22:13:16 +0800

    添加babel来转译es6

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import _ from 'lodash'
-import './style.css'
+import './style.less'
 import Img from './dlam.jpg'
 
 function component() {
-    var element = document.createElement('div');
+    let element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     //Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
     
-    var myImg = new Image()
+    let myImg = new Image()
     myImg.src = Img
+    myImg.classList.add('img')
     element.appendChild(myImg)
 
     return element;
   }
   
   document.body.appendChild(component());
+
+  console.log("hello world")
+
+  let array1 = [1,2,3]
+  let array2 = array1.map((n) => n+1)
+  console.log(array2)
--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,33 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
-      }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+    }
     ]
   }
 };
