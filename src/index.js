5f935d9b9c07c7c0f07e834d8899afcd71b67cd6
kiterumer <1404646092@qq.com>
2019-05-07 12:01:51 +0800

    添加新的入口文件

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import _ from 'lodash'
 import './style.less'
 import Img from './dlam.jpg'
+import printMe from './print'
 
 function component() {
     let element = document.createElement('div');
+
+    let btn = document.createElement('button')
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     //Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
     
+    btn.innerHTML = 'Click me'
+    btn.onclick = printMe
+
+    element.appendChild(btn)
+
     let myImg = new Image()
     myImg.src = Img
     myImg.classList.add('img')
